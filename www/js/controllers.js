a61f486c5b2059ea86233737540316ab7168dd98
ozge halil duman <ozgehalilduman@gmail.com>
2016-10-24 16:10:36 +0300

    24-10-2016 Dersde islenenler
    24 10 2016 dersinde yapılanlar...
    yatay ve dikey menu ornekleri eklendi. css ornekleri var

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -60,12 +60,12 @@ angular.module('starter.controllers', [])
     {ad:'murat',soyad:'AKALAN',okulno:'2011'}
   ];
   $scope.kategoriler = [
-    { baslik: 'KAHVALTILIKLAR', id: 1 },
-    { baslik: 'APERATİFLER', id: 2 },
-    { baslik: 'ÇORBALAR', id: 3 },
-    { baslik: 'SALATALAR', id: 4 },
-    { baslik: 'İÇECEKLER', id: 5 },
-    { baslik: 'TATLILAR', id: 6 }
+    { baslik: 'KAHVALTILIKLAR', id: 1,resim:"kahve.png",arkaplan:"kahvalti_bg.jpg"},
+    //{ baslik: 'APERATİFLER', id: 2 },
+    { baslik: 'ÇORBALAR', id: 3,resim:"corba.png",arkaplan:"corba_bg.jpg"},
+    { baslik: 'SALATALAR', id: 4,resim:"salata.png",arkaplan:"salata_bg.jpg" },
+    //{ baslik: 'İÇECEKLER', id: 5 },
+    { baslik: 'TATLILAR', id: 6,resim:"tatli.png",arkaplan:"tatli_bg.jpg" }
   ];
 
 })
